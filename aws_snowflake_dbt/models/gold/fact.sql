3204d4a826d1f9cffde0efb501b4612a2e483fef
Rellikson <kisyular@msu.edu>
2026-01-22 16:58:16 -0500

    ADD COMMENTS TO THE FACT.SQL

--- aws_snowflake_dbt/models/gold/fact.sql
+++ aws_snowflake_dbt/models/gold/fact.sql
@@ -30,3 +30,19 @@ from
             on {{ config["join_condition"] }}
     {% endif %}
 {% endfor %}
+
+/*
+When the code above is compiled, it will look like this:
+select
+    obt.booking_id, obt.listing_id, obt.host_id, obt.total_amount, obt.service_fee, 
+    obt.cleaning_fee, obt.accommodates, obt.bedrooms, obt.bathrooms, 
+    obt.price_per_night, obt.response_rate
+from
+    airbnb.gold.obt as obt
+left join
+    airbnb.gold.dim_listings as dim_listings
+    on obt.listing_id = dim_listings.listing_id
+left join
+    airbnb.gold.dim_hosts as dim_hosts
+    on obt.host_id = dim_hosts.host_id
+*/
